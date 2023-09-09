f5c9edaffcf3cdba4f6f2d7588440fe85992220c
Conor McKnight <C0nw0nks@googlemail.com>
2023-09-09 12:41:17 +0100

    Update H265_LargestFileFirst.cmd

--- H265_LargestFileFirst.cmd
+++ H265_LargestFileFirst.cmd
@@ -13,7 +13,7 @@ set root_path="%~dp0"
 
 ::start powershell code
 echo $extensions = @("*.mkv", "*.mp4", "*.avi") >"%root_path:"=%%~n0.ps1"
-echo Get-ChildItem "%plex_folder:"=%" -Include $extensions -Recurse ^| sort -descending -property length ^| select -first %search_limit% name -ExpandProperty Name ^| Out-File -FilePath "%root_path:"=%%~n0.txt" -NoClobber -Append >>"%root_path:"=%%~n0.ps1"
+echo Get-ChildItem "%plex_folder:"=%" -Include $extensions -Recurse ^| sort -descending -property length ^| select -first %search_limit% name -ExpandProperty FullName ^| Out-File -FilePath "%root_path:"=%%~n0.txt" -NoClobber -Append >>"%root_path:"=%%~n0.ps1"
 ::end powershell code
 powershell -ExecutionPolicy Unrestricted -File "%root_path:"=%%~n0.ps1" "%*" -Verb runAs
 del "%root_path:"=%%~n0.ps1"
@@ -21,10 +21,10 @@ del "%root_path:"=%%~n0.ps1"
 :: start /b /wait "%root_path:"=%PlexCleaner_265.cmd" "%plex_folder:"=%\%%i" "0" "0" "0"
 for /f "delims=" %%i in ('Type "%root_path:"=%%~n0.txt"') do (
 	echo "%plex_folder:"=%\%%i"
-	for /f %%b in ('%root_path:"=%win-x64\Tools\FfMpeg\bin\ffmpeg.exe -i "%plex_folder:"=%\%%i" 2^>^&1 ^| find /c /i "Video: hevc "') do (
+	for /f %%b in ('%root_path:"=%win-x64\Tools\FfMpeg\bin\ffmpeg.exe -i "%%i" 2^>^&1 ^| find /c /i "Video: hevc "') do (
 		if [%%b]==[0] (
 			echo "%%i" is not hevc encoded
-			call "%root_path:"=%PlexCleaner_265.cmd" "%plex_folder:"=%\%%i" "0" "0" "0"
+			call "%root_path:"=%PlexCleaner_265.cmd" "%%i" "0" "0" "0"
 		) else (
 			echo "%%i" is hevc encoded
 		)
